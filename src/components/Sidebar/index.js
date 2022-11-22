c1c444a0f84534d57044fe6c0e55cdec1e5565f0
Alexandra Kinsman <alexandrakinsman@Alexs-Mac.lan>
2022-11-22 15:11:17 -0700

    image upload and sidebar development

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+import LogoS from '../../assets/images'
+
+const Sidebar = () => {
+  <div className='nav-bar'>
+    <Link className='logo' to='/'>
+      <img src={}
+    </Link>
+  </div>
+}
+
+export default Sidebar
