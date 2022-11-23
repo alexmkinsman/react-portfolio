11d1d4dd7442c749ada59c95f79a4022ba144f12
Alexandra Kinsman <alexandrakinsman@Alexs-Mac.lan>
2022-11-23 15:10:05 -0700

    h1 style/animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = [' ','A', 'l', 'e', 'x']
-  const jobArray = ['a', ' ','w', 'e','b', ' ', 'd','e','v','e','l','o','p','e','r']
+  const nameArray = [' ','A', 'l', 'e', 'x', '!']
+  const jobArray = ['A', ' ','w', 'e','b', ' ', 'd','e','v','e','l','o','p','e','r', '.']
+  const hiArray = ['H', 'i', ',']
+  const imArray = ['I', "'", 'm']
+  
+  useEffect(() => {
+    setTimeout(()=> {
+      return setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+  
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass}_12`}>i,</span>
+        <AnimatedLetters letterClass={letterClass}
+        strArray={hiArray}
+        idx={9} />
           <br/> 
-          <span className={`${letterClass}_13`}>I</span>
-          <span className={`${letterClass}_14`}>'m</span>
+          <AnimatedLetters letterClass={letterClass}
+        strArray={imArray}
+        idx={11} />
         <AnimatedLetters letterClass={letterClass}
         strArray={nameArray}
         idx={15} />
