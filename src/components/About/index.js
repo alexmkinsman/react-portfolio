6dd73fcc4e1aaed4b5471eca5a199215c726cd6f
Alexandra Kinsman <alexandrakinsman@Alexs-Mac.lan>
2022-11-28 12:58:45 -0700

    About me title added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,19 @@
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters'
 
-const About = () => ('Hello')
+const About = () => {
+  return (
+    <div className='containter about-page'>
+      <div className='text-zone'>
+        <h1>
+          <AnimatedLetters
+            strArray={['A','b','o','u','t',' ','m','e']}
+            idx={15}
+          />
+        </h1>
+      </div>
+    </div>
+  )
+}
 
 export default About
